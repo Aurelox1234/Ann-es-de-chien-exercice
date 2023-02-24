576c7c6889a64f392ee6b77045f45af1e188015a
aurelox1234 <adubois1307@gmail.com>
2023-02-24 16:11:00 +0100

    annes canines

---
+++ index.js
@@ -0,0 +1,11 @@
+let myAge = 13;
+//Mon age est de 13 ans.
+let earlyYears = 2;
+//C'est l'age de départ.
+earlyYears *=10,5;
+let laterYears = myAge-2;
+laterYears *= 4;
+const myAgeInDogYears = laterYears+earlyYears;
+//Mon age en années cannines correspond à laterYears + earlyYears.
+const myName = "AURELIEN".toLowerCase();
+console.log(`Je m'appelle ${myName}.J'ai ${myAge} ans, ce qui est égal à ${myAgeInDogYears} ans en années canines.`);
